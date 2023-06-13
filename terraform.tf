3ad8db66324ee7f2729e5782b57dd6ec50bf90f2
Thimo Limpert (q.beyond AG) <thimo.limpert@qbeyond.de>
2023-06-13 10:00:00 +0200

    no provider block should be present in modules

--- terraform.tf
+++ terraform.tf
@@ -1,12 +1,8 @@
 terraform {
   required_providers {
-    # azurerm = {
-    #   source  = "hashicorp/azurerm"
-    #   version = ">= 3.7.0"
-    # }
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = ">= 3.7.0"
+    }
   }
 }
-
-# provider "azurerm" {
-#   features {}
-# }
